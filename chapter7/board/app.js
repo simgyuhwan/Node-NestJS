4d1540f1bb0873ff4e4d4c8bbc5e59268b27f25d
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2023-12-30 20:10:04 +0900

    게시글 상세보기 구현

--- chapter7/board/app.js
+++ chapter7/board/app.js
@@ -17,7 +17,17 @@ app.use(express.urlencoded({extended: true}));
 app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const page = parseInt(req.query.page) || 1;
+  const search = req.query.search || "";
+  try {
+    const [posts, paginator] = await postService.list(collection, page, search);
+
+    res.render("home", { title: "테스트 게시판", search, paginator, posts});
+    return;
+  } catch (error) {
+    console.error(error);
+  }
   res.render("home", { title: "테스트 게시판"});
 });
 
@@ -32,8 +42,11 @@ app.post("/write", async (req, res) => {
 });
 
 app.get("/detail/:id", async (req, res) => {
+  const result = await postService.getDetailPost(collection, req.params.id);
+  
   res.render("detail", {
     title: "테스트 게시판",
+    post: result,
   });
 });
 
--- chapter7/board/services/post-service.js
+++ chapter7/board/services/post-service.js
@@ -1,3 +1,35 @@
+const paginator = require("../utils/paginator");
+const { ObjectId } = require("mongodb");
+
+const projectionOption = {
+  projection: {
+    password: 0,
+    "comments.password": 0,
+  },
+};
+
+async function getDetailPost(collection, id) {
+  return await collection.findOneAndUpdate({ _id: new ObjectId(id) }, { $inc: { hits: 1} }, projectionOption);
+}
+
+// 글 목록
+async function list(colleciton, page, search) {
+  const perPage = 10;
+
+  const query = {title: new RegExp(search, "i")};
+
+  const cursor = colleciton.find(query, {limit: perPage, skip: (page - 1) * perPage}).sort({
+    createdDt: -1,
+  });
+
+  const totalCount = await colleciton.count(query);
+  const posts = await cursor.toArray();
+
+  const paginatorObj = paginator({totalCount, page, perPage: perPage});
+  return [posts, paginatorObj];
+}
+
+// 글쓰기
 async function writePost(collection, post) {
   post.hits = 0;
   post.createDt = new Date().toISOString();
@@ -5,5 +37,7 @@ async function writePost(collection, post) {
 }
 
 module.exports = {
+  list,
   writePost,
+  getDetailPost,
 };
