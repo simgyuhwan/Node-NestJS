b15f80cb4566ab648313a630398ebc49187e8ad6
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-01-03 11:41:28 +0900

    블로그 시작

---
+++ chapter8/blog/src/blog.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Param, Body, Delete, Get, Post, Put } from '@nestjs/common';
+import { BlogService } from './blog.service';
+
+@Controller('blog')
+export class BlogController {
+  blogService: BlogService
+
+  constructor() {
+    this.blogService = new BlogService();
+  }
+
+  @Get()
+  getAllPosts() {
+    console.log('모든 게시글 가져오기');
+    return this.blogService.getAllPosts();
+  }
+
+  @Post()
+  createPost(@Body() postDto) {
+    console.log('게시글 작성');
+    this.blogService.createPost(postDto)
+    return 'success';    
+  }
+
+  @Get('/:id')
+  getPost(@Param('id') id: string) {
+    console.log(`[id: ${id}]게시글 하나 가져오기`);
+    return this.blogService.getPost(id);
+  }
+
+  @Delete('/:id')
+  deletePost(@Param('id') id: string) {
+    console.log('게시글 삭제');
+    this.blogService.delete(id);
+    return 'success';
+  }
+
+  @Put('/:id')
+  updatePost(@Param('id') id: string, @Body() postDto) {
+    console.log(`[${id}] 게시글 업데이트`, id, postDto);
+    return this.blogService.updatePost(id, postDto);
+  }
+
+}
