17d23328665bb722e7afca980cb923022be8487a
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-01-05 00:48:51 +0900

    환경 변수 설정

---
+++ chapter9/config-test/src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config'
+
+@Controller()
+export class AppController {
+  constructor(private configService: ConfigService) {}
+
+  @Get()
+  getHello(): string {
+    const message = this.configService.get('MESSAGE');
+    return message;
+  }
+}
