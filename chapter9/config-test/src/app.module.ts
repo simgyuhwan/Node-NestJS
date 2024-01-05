d9c0c36a769b1736779eeefa4de99cbf571b48b2
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-01-05 09:06:27 +0900

    환경 변수 설정의 모든 것

--- chapter9/config-test/src/app.module.ts
+++ chapter9/config-test/src/app.module.ts
@@ -3,9 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
+import config from './configs/config';
+
+console.log('env : ' + process.env.NODE_ENV);
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
+  imports: [ConfigModule.forRoot({ 
+    isGlobal: true,
+    envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env` ,
+    load: [config],
+  }),
+   WeatherModule],
   controllers: [AppController],
   providers: [AppService],
 })
