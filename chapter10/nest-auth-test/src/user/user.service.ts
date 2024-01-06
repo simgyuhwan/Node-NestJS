3fbc0665cab9b1346bbafa103d0bba9b7b9b52bb
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-01-06 21:00:03 +0900

    입력값 유효성 검증 추가

--- chapter10/nest-auth-test/src/user/user.service.ts
+++ chapter10/nest-auth-test/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto, UpdateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
@@ -9,7 +10,7 @@ export class UserService {
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
-  createUser(user: User): Promise<User> {
+  createUser(user: CreateUserDto): Promise<User> {
     return this.userRepository.save(user);
   }
 
@@ -20,7 +21,7 @@ export class UserService {
     return result;
   }
 
-  async updateUser(email: string, _user: User) {
+  async updateUser(email: string, _user: UpdateUserDto) {
     const user: User = await this.getUser(email);
     console.log(_user);
 
