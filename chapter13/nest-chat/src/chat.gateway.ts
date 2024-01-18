9f2dc34b268d910cd91bb0d20bf99d23b9dc7aff
simgyuhwan <37525159+simgyuhwan@users.noreply.github.com>
2024-01-18 20:53:46 +0900

    chat 구현

---
+++ chapter13/nest-chat/src/chat.gateway.ts
@@ -0,0 +1,13 @@
+import { SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway {
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('message')
+  handleMessage(socket: Socket, data: any): void {
+    console.log(socket.id);
+    this.server.emit('message', `client-${socket.id.substring(0, 4)} : ${data}`,);
+  }
+}
